37ebb5e4f4a8419fe55040aa71c7c50d1b621004
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2022-06-27 17:40:20 +0530

    improved form page

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./new.scss"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 const New = () => {
   return (
     <div className='new'>
@@ -13,14 +14,20 @@ const New = () => {
         </div>
         <div className="bottom">
           <div className="left">
-            <img src=" https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="" />
+            <img src=" https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" className='icon' />
           </div>
           <div className="right">
             <form>
               <div className="formInput">
+                <label htmlFor='file'>Image:<DriveFolderUploadOutlinedIcon className='icon' /></label>
+                <input type="file" id='file' style={{ display: "none" }} />
+              </div>
+
+              <div className="formInput">
                 <label>Username</label>
                 <input type="text" placeholder='John Doe' />
               </div>
+
               <div className="formInput">
                 <label>First and Last Name</label>
                 <input type="text" placeholder='John Doe' />
@@ -45,6 +52,7 @@ const New = () => {
                 <label>Password</label>
                 <input type="password" />
               </div>
+              <button>Send</button>
             </form>
           </div>
         </div>
